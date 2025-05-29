4fad6a610eafce819e8d819bf6a844ab95ba38f6
EmranHossen <emranhossen3075@gmail.com>
2025-05-29 20:56:36 +0600

    chore : Query String

--- Section_03_HTTP/HTTP/HTTPExamples/Program.cs
+++ Section_03_HTTP/HTTP/HTTPExamples/Program.cs
@@ -3,13 +3,14 @@ var app = builder.Build();
 
 app.Run(async (HttpContext context) =>
 {
-    string path = context.Request.Path;
-    string methodName = context.Request.Method;
-
     context.Response.Headers["Content-type"] = "text/html";
-    await context.Response.WriteAsync($"<p> {path} </p>");
-    await context.Response.WriteAsync($"<p> {methodName} </p>");
-
+    if (context.Request.Method == "GET")
+    {
+        if (context.Request.Query.ContainsKey("id"))
+        {
+            string id = context.Request.Query["Id"];
+            await context.Response.WriteAsync($"<p> {id} </p>");
+        }
+    }
 });
-
 app.Run();
