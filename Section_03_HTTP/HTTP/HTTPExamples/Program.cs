57396e1fa322ead8363c76e0a4ef9b716510a424
EmranHossen <emranhossen3075@gmail.com>
2025-05-29 17:31:43 +0600

    Chore : Get Request

--- Section_03_HTTP/HTTP/HTTPExamples/Program.cs
+++ Section_03_HTTP/HTTP/HTTPExamples/Program.cs
@@ -3,10 +3,13 @@ var app = builder.Build();
 
 app.Run(async (HttpContext context) =>
 {
-    context.Response.Headers["Server"] = "My Server";
-    context.Response.Headers["Mykey"] = "My value";
-    context.Response.Headers["Content-Type"] = "text/html";
-    await context.Response.WriteAsync("<h1> Hello World </h1>");
+    string path = context.Request.Path;
+    string methodName = context.Request.Method;
+
+    context.Response.Headers["Content-type"] = "text/html";
+    await context.Response.WriteAsync($"<p> {path} </p>");
+    await context.Response.WriteAsync($"<p> {methodName} </p>");
+
 });
 
 app.Run();
