a70acf63b1d0ce0b006ff76b8bb9057d9178fee1
EmranHossen <emranhossen3075@gmail.com>
2025-05-30 16:58:11 +0600

    chore : Custom Middleware Class

---
+++ 03.Middleware/MiddlewareExample/CustomMiddleware/MyCustomMiddleware.cs
@@ -0,0 +1,14 @@
+﻿
+namespace MiddlewareExample.CustomMiddleware
+{
+    public class MyCustomMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("My custom Middleware - Starts");
+            await next(context);
+
+            await context.Response.WriteAsync("My custom Middleware - End");
+        }
+    }
+}
--- 03.Middleware/MiddlewareExample/Program.cs
+++ 03.Middleware/MiddlewareExample/Program.cs
@@ -1,30 +1,31 @@
+using MiddlewareExample.CustomMiddleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTransient<MyCustomMiddleware>();
+
 var app = builder.Build();
 
 app.Use(async (HttpContext context, RequestDelegate next) =>
 {
-    await context.Response.WriteAsync("Hello ");
+    await context.Response.WriteAsync("From Middleware 1");
     await next(context);
 
 });
 
-app.Use(async (HttpContext context, RequestDelegate next) =>
-{
-    await context.Response.WriteAsync("Hello2 ");
-    await next(context);
-
-});
+// middleware 2
+app.UseMiddleware<MyCustomMiddleware>();
 
 app.Use(async (HttpContext context, RequestDelegate next) =>
 {
-    await context.Response.WriteAsync("Hello3 ");
+    await context.Response.WriteAsync("From Middleware 3");
     await next(context);
 
 });
 
 app.Run(async (HttpContext context) =>
 {
-    await context.Response.WriteAsync("Hello Again");
+    await context.Response.WriteAsync("From Middleware 4");
 
 });
 
