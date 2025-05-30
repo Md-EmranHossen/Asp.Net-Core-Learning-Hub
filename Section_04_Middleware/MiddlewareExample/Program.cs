02095f61905c4ac4405588ff807ca392a964c2d1
EmranHossen <emranhossen3075@gmail.com>
2025-05-30 12:56:14 +0600

    chore : MiddleWare Chain

--- Section_04_Middleware/MiddlewareExample/Program.cs
+++ Section_04_Middleware/MiddlewareExample/Program.cs
@@ -1,11 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("Hello ");
+    await next(context);
 
+});
+
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("Hello2 ");
+    await next(context);
+
+});
+
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("Hello3 ");
+    await next(context);
+
+});
 
 app.Run(async (HttpContext context) =>
 {
-    await context.Response.WriteAsJsonAsync("Hello Again");
+    await context.Response.WriteAsync("Hello Again");
 
 });
 
