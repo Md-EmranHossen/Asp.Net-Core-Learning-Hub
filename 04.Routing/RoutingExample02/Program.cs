d20edcc66a5d6b2b94c6555e410c4943330aa1bc
EmranHossen <emranhossen3075@gmail.com>
2025-05-31 09:16:34 +0600

    feat : Map Method

---
+++ 04.Routing/RoutingExample02/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.Map("map1", async (context) =>
+    {
+        await context.Response.WriteAsync("In Map 1");
+    });
+
+    endpoints.Map("map2", async (context) =>
+    {
+        await context.Response.WriteAsync("In Map 2");
+    });
+
+});
+
+app.Run(async context =>
+{
+    await context.Response.WriteAsync($"Request received at {context.Request.Path}");
+});
+
+app.Run();
