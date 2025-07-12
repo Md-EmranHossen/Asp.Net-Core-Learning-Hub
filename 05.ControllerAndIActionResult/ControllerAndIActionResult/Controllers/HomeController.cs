06c017ed7ba7c677a309fbb813db2769db073d79
EmranHossen <emranhossen3075@gmail.com>
2025-07-12 20:23:52 +0600

    feat : Creating Controller

---
+++ 05.ControllerAndIActionResult/ControllerAndIActionResult/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllerAndIActionResult.Controllers
+{
+   public class HomeController : Controller
+    {
+        [Route("sayhello")]
+        public string methood1()
+        {
+            return "Hello From method1";
+        }
+    }
+}
