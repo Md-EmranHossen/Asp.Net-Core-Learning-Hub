28ce2c4e97a47f53e30f54a9029ab00771f1ee0d
EmranHossen <emranhossen3075@gmail.com>
2025-07-18 16:40:26 +0600

    File Result

--- 05.ControllerAndIActionResult/ControllerAndIActionResult/Controllers/HomeController.cs
+++ 05.ControllerAndIActionResult/ControllerAndIActionResult/Controllers/HomeController.cs
@@ -3,32 +3,33 @@ using ControllerAndIActionResult.Models;
 
 namespace ControllerAndIActionResult.Controllers
 {
-   public class HomeController : Controller
+    public class HomeController : Controller
     {
-        [Route("home")]
-       
-        public ContentResult Index()
+        [Route("file-download")]
+        public VirtualFileResult FileDownload()
         {
-            /*
-             return new ContentResult()
-              {
-                  Content = "Hello From Index",
-                  ContentType = "text/plain"
-              };
-             */
-
-            //  return Content("Hello from Index", "text/plain");
-
-            return Content("<h1> Welcome </h1> <h2>Hello from Index</h2>","text/html");
-        }
-        [Route("/")]
-        [Route("person")]
-        public JsonResult Person()
-        {
-            Person person = new Person() { Id = Guid.NewGuid(), FirstName = "Emran", LastName = "Hossen", Age = 25 };
-            // return new JsonResult(person);
-            return Json(person);
+           // return new VirtualFileResult("/FirstMonth.pdf","application/pdf");
+            return File("/FirstMonth.pdf", "application/pdf");
         }
 
+
+
+        [Route("file-download2")]
+        public PhysicalFileResult FileDownload2()
+        {
+            //return new PhysicalFileResult(@"c:\aspnetcore\FirstMonth.pdf", "application/pdf");
+            return  PhysicalFile(@"c:\aspnetcore\FirstMonth.pdf", "application/pdf");
+
+        }
+
+
+
+        [Route("file-download3")]
+        public FileContentResult FileDownload3()
+        {
+          byte[] bytes =  System.IO.File.ReadAllBytes(@"c:\aspnetcore\FirstMonth.pdf");
+            // return new FileContentResult(bytes, "application/pdf");
+            return File(bytes, "application/pdf");
+        }
     }
-}
+} 
--- 05.ControllerAndIActionResult/ControllerAndIActionResult/Program.cs
+++ 05.ControllerAndIActionResult/ControllerAndIActionResult/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 var app = builder.Build();
+app.UseStaticFiles();
 
 app.MapControllers();
 
