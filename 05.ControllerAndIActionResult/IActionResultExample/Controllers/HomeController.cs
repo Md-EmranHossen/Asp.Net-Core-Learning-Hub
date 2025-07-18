6c2e3581bd74c43ed281f5db073e55c3871d9374
EmranHossen <emranhossen3075@gmail.com>
2025-07-18 18:24:27 +0600

    status code result

--- 05.ControllerAndIActionResult/IActionResultExample/Controllers/HomeController.cs
+++ 05.ControllerAndIActionResult/IActionResultExample/Controllers/HomeController.cs
@@ -9,34 +9,35 @@ namespace IActionResultExample.Controllers
             // Book id should be applied
             if (!Request.Query.ContainsKey("bookid"))
             {
-                Response.StatusCode = 400;
-                return Content("Book id is not supplied");
+                return BadRequest("Book is not supplied");
             }
 
             // Book id cna't be empty
             if (string.IsNullOrEmpty(Convert.ToString(Request.Query["bookid"]))){
-                return Content("Book id can't be null or empty");
+                return BadRequest("Book id can't be null or empty");
             }
+
 
             // Book is should be between 1 to 100
             int bookId = Convert.ToUInt16(ControllerContext.HttpContext.Request.Query["Bookid"]);
 
             if(bookId <= 0)
             {
-                return Content("Book id can not be less then zero");
+                return BadRequest("Book id can not be less then zero");
             }
 
             if(bookId > 1000)
             {
-                return Content("Book id can't be greater than 1000");
+                return NotFound("Book id can't not be greater than 1000");
             }
 
             // isloggedin should be true
             if(Convert.ToBoolean(Request.Query
                 ["isloggedin"]) == false)
             {
-                Response.StatusCode = 401;
-                return Content("User must be authenticated");
+
+                //   return Unauthorized("User must be authenticated");
+                return StatusCode(401);
             }
 
             return File("/FirstMonth.pdf","application/pdf");
