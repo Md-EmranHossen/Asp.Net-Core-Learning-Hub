9187da6f4276891d323a2b5aabebc704eb250f97
EmranHossen <emranhossen3075@gmail.com>
2025-07-21 08:10:03 +0600

    add files

---
+++ 05. Controllers & IActionResult/08. Redirect Results - Part 2/IActionResultExample/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using IActionResultExample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IActionResultExample.Controllers
+{
+  public class HomeController : Controller
+  {
+    [Route("bookstore/{bookId?}/{isloggedin?}/{bookName?}")]
+    //Url: /bookstore?bookid=10&isloggedin=true
+    public IActionResult Index( int? bookId,[FromRoute] bool? isloggedin,Book book)
+    {
+      
+      if (bookId == null)
+      {
+        return BadRequest("Book id is not supplied or null");
+      }
+
+      //Book id should be between 1 to 1000
+      if (bookId <= 0)
+      {
+        return BadRequest("Book id can't be less than or equal to zero");
+      }
+      if (bookId > 1000)
+      {
+        return NotFound("Book id can't be greater than 1000");
+      }
+
+      //isloggedin should be true
+      if (isloggedin == false)
+      {
+        //return Unauthorized("User must be authenticated");
+        return StatusCode(401);
+      }
+
+      return Content($"Book id: {bookId}","text/plain");
+    
+    }
+  }
+}
--- 05.ControllerAndIActionResult/IActionResultExample/Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddConnections();
-
-var app = builder.Build();
-
-app.UseStaticFiles();
-app.UseRouting();
-app.MapControllers();
-app.Run();
